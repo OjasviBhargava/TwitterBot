8e6d4f8898c5bc052a9aa2cd1c38ed98fed6e07e
OjasviBhargava <34980131+OjasviBhargava@users.noreply.github.com>
2018-03-08 19:37:58 +0530

    added bot,js

--- bot.js
+++ bot.js
@@ -1,1 +1,24 @@
+console.log("The bottttt iss startingggggg!!!!!!");
 
+var Twit = require('twit');
+
+//path of config.js
+var config = require('./config');
+var T = new Twit(config);
+  
+//the message we want to post
+var tweet_message = {
+  status: 'My First Tweet using #TwitterAPI and #NodeJS'
+}
+
+//This posts our tweet
+T.post('statuses/update', tweet_message, tweetIt);
+
+//our callback function
+function tweetIt(err, data, response){
+  if (err) {
+    console.log("Something went wrong : " + err);
+  } else {
+    console.log(tweet);
+  }
+}
