0c54c6ab51eb0b41ab72bbba2e2f52999d2a9ec3
OjasviBhargava <34980131+OjasviBhargava@users.noreply.github.com>
2018-03-08 19:37:10 +0530

    added config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+  consumer_key:         'Enter your API key',
+  consumer_secret:      'Enter your API Secret',
+  access_token:         'Your Access Token',
+  access_token_secret:  'Your Access Token Secret',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
